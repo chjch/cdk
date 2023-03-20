9fff75d1c11ecc2fde2ed29cc8ad3094c92d555c
Changjie Chen <chj.chen@Hotmail.com>
2023-03-20 09:53:49 -0400

    add hover over popups

--- app/assets/customscript.js
+++ app/assets/customscript.js
@@ -1,80 +1,119 @@
-// window.theme = {
-//     accent:"#004172",
-//     accent_negative:"#ff2c6d",
-//     accent_positive:"#33ffe6",
-//     background_content:"#edf3f4",
-//     background_page:"#d6e4ea",
-//     border:"#8EA9C1",
-//     breakpoint_font:"1200px",
-//     breakpoint_stack_blocks:"700px",
-//     card_border:{
-//         width:"0px",
-//         style:"solid",
-//         color:"#8EA9C1",
-//         radius:"10px"
-//     },
-//     card_background_color:"#edf3f4",
-//     card_box_shadow:"0px 1px 3px rgba(0,0,0,0.12), 0px 1px 2px rgba(0,0,0,0.24)",
-//     card_margin:"15px",
-//     card_padding:"5px",
-//     card_outline:{
-//         width:"0px",
-//         style:"solid",
-//         color:"#8EA9C1"
-//     },
-//     card_header_border:{
-//         width:"0px",
-//         style:"solid",
-//         color:"#8EA9C1",
-//         radius:"0px"
-//     },
-//     card_header_background_color:"#edf3f4",
-//     card_header_box_shadow:"0px 0px 0px rgba(0,0,0,0)",
-//     card_header_margin:"0px",
-//     card_header_padding:"10px",
-//     colorway:[
-//         "#119dff",
-//         "#66c2a5",
-//         "#fc8d62",
-//         "#e78ac3",
-//         "#a6d854",
-//         "#ffd92f",
-//         "#e5c494",
-//         "#b3b3b3"
-//     ],
-//     colorscale:[
-//         "#1f78b4",
-//         "#4786bc",
-//         "#6394c5",
-//         "#7ba3cd",
-//         "#92b1d5",
-//         "#a9c0de",
-//         "#bed0e6",
-//         "#d4dfee",
-//         "#eaeff7",
-//         "#ffffff"
-//     ],
-//     font_family:"Open Sans",
-//     font_size:"17px",
-//     font_size_smaller_screen:"15px",
-//     font_family_header:"Open Sans",
-//     font_size_header:"24px",
-//     font_family_headings:"Open Sans",
-//     font_headings_size:null,
-//     header_border:{
-//         width:"0px",
-//         style:"solid",
-//         color:"#8EA9C1",
-//         radius:"0px"
-//     },
-//     header_background_color:"#edf3f4",
-//     header_box_shadow:"0px 1px 3px rgba(0,0,0,0.12), 0px 1px 2px rgba(0,0,0,0.24)",
-//     header_margin:"0px 0px 15px 0px",
-//     header_padding:"0px",
-//     text:"#718BA5",
-//     report_font_family:"Computer Modern",
-//     report_font_size:"12px",
-//     report_background_page:"white",
-//     report_background_content:"#FAFBFC",
-//     report_text:"black"
+const legendTips = {
+    "MHHW": "Mean Higher High Water",
+    "EWL1R": "Extreme Water Level 1-year Return",
+    "EWL2R": "Extreme Water Level 2-year Return",
+    "EWL10R": "Extreme Water Level 10-year Return",
+    "NFHL100": "FEMA 100-year Flood",
+    "CAT1": "Category-1 Hurricane",
+    "CAT3": "Category-3 Hurricane",
+    "CAT5": "Category-5 Hurricane"
+};
+
+const legendPopup = (item) => {
+    "use strict";
+
+    item.addEventListener('mouseover', (event) => {
+        // Get the element that triggered the event
+        const hoveredElement = event.target;
+        const parentElement = hoveredElement.parentNode;
+
+        const popup = document.createElement('div');
+        popup.setAttribute('id', 'legend_popup');
+        popup.textContent = legendTips[parentElement.children[0].innerHTML];
+        popup.classList.add('popup');
+
+        const parentPos = parentElement.getBoundingClientRect();
+        const popupMessageTop = parentPos.top - 10;
+        const popupMessageLeft = parentPos.left + parentPos.width + 5;
+        popup.style.top = `${popupMessageTop}px`;
+        popup.style.left = `${popupMessageLeft}px`;
+        document.body.appendChild(popup);
+    });
+
+    item.addEventListener("mouseout", () => {
+        const popup = document.getElementById("legend_popup");
+        document.body.removeChild(popup);
+    });
+};
+
+const sliderPopup = (item) => {
+    "use strict";
+
+    item.addEventListener('mouseover', (event) => {
+        // Get the element that triggered the event
+        const hoveredElement = event.target;
+
+        const popup = document.createElement('div');
+        popup.setAttribute('id', 'slider_popup');
+        popup.textContent = legendTips[hoveredElement.innerHTML];
+        popup.classList.add('popup');
+
+        const popup_hidden_span = document.createElement('span');
+        popup_hidden_span.style.visibility = "hidden";
+        popup_hidden_span.innerHTML = popup.textContent;
+        document.body.appendChild(popup_hidden_span);
+
+        const hoveredPos = hoveredElement.getBoundingClientRect();
+        const popupMessageTop = hoveredPos.top - 10;
+        const popupMessageLeft = hoveredPos.left - hoveredPos.width - popup_hidden_span.offsetWidth;
+        document.body.removeChild(popup_hidden_span);
+
+        popup.style.top = `${popupMessageTop}px`;
+        popup.style.left = `${popupMessageLeft}px`;
+        document.body.appendChild(popup);
+    });
+
+    item.addEventListener("mouseout", () => {
+        const popup = document.getElementById("slider_popup");
+        document.body.removeChild(popup);
+    });
+};
+
+const addListeners = () => {
+    "use strict";
+    const lineChart = document.getElementById("line-chart");
+    const legendItems = lineChart.getElementsByClassName("traces");
+
+    for (let i = 0; i < legendItems.length; i++) {
+        legendPopup(legendItems[i]);
+    }
+
+    const ySlider = document.getElementById("map-y-slider");
+    const marks = ySlider.getElementsByClassName("rc-slider-mark-text");
+
+    for (let i = 0; i < marks.length; i++) {
+        sliderPopup(marks[i]);
+    }
+};
+
+// function addListeners() {
+//     "use strict";
+//
+//     const lineChart = document.getElementById("line-chart");
+//     const legendItems = lineChart.getElementsByClassName("traces");
+//
+//     // const elements = document.getElementsByClassName("groups");
+//
+//     // Loop through the elements and add the event listener
+//     for (let i = 0; i < legendItems.length; i++) {
+//         legendItems[i].addEventListener('mouseover', (event) => {
+//             console.log(legendItems[i])
+//             // Get the element that triggered the event
+//             const hoveredElement = event.target;
+//
+//             // console.log(hoveredElement);
+//
+//             const parentElement = hoveredElement.parentNode;
+//
+//             console.log(parentElement)
+//
+//             // Do something with the hovered element
+//             console.log(legendTips[parentElement.children[0].innerHTML]);
+//         });
+//     }
 // }
+
+//
+// document.addEventListener("DOMContentLoaded", function() {
+//     setTimeout(function() {addListeners()}, 1000);
+// });
