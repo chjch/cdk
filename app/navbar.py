953e93e4f558e45e018ea89d8fc3275801e89ed3
Changjie Chen <chj.chen@Hotmail.com>
2023-08-22 23:01:20 -0400

    update navbar style

--- app/navbar.py
+++ app/navbar.py
@@ -9,10 +9,18 @@ navbar_brand = html.A(
     dbc.Row(
         [
             dbc.Col(html.Img(src=NAVBAR_LOGO, height="40px")),
-            dbc.Col(dbc.NavbarBrand(NAVBAR_BRAND_TITLE,
-                                    className="ms-2",
-                                    style={"font-size": "1.6em",
-                                           "padding-left": "20px"})),
+            dbc.Col(
+                dbc.NavbarBrand(
+                    NAVBAR_BRAND_TITLE,
+                    className="ms-2",
+                    style={
+                        "font-size": "1.6em",
+                        "font-family": "poppins",
+                        "padding-left": "20px",
+                        "color": "#0E3183",
+                    },
+                )
+            ),
         ],
         align="center",
         className="g-0",
@@ -22,42 +30,34 @@ navbar_brand = html.A(
 )
 
 
-def navbar_link(name: str, href: str):
+def navbar_btn(name: str, href: str):
     return dbc.Col(
         dbc.Button(
-            name, color="primary",
+            name,
+            color="primary",
             id=f"{name.replace(' ', '').lower()}-button",
-            href=f'/viewer/{href}',
-            className="ms-2 rounded-pill btn", n_clicks=0,
+            href=f"/viewer/{href}",
+            className="ms-2 rounded-pill btn",
+            n_clicks=0,
         ),
         width="auto",
     )
 
 
-overview_link = navbar_link('Housing',
-                            'housing')
-transport_link = navbar_link('Transportation',
-                             'transportation')
-housing_link = navbar_link('Critical Infrastructure',
-                           'critical-infrastructure')
-community_link = navbar_link('Community Services',
-                             'community-services')
-resource_link = navbar_link('Natural & Cultural Resources',
-                            'natural-cultural-resources')
-tourist_link = navbar_link('Local Economy',
-                           'local-economy')
-
 navbar_right = dbc.Row(
     [
-        overview_link,
-        transport_link,
-        housing_link,
-        community_link,
-        resource_link,
-        tourist_link
+        navbar_btn("Housing", "housing"),
+        navbar_btn("Transportation", "transportation"),
+        navbar_btn("Critical Infrastructure", "critical-infrastructure"),
+        navbar_btn("Community Services", "community-services"),
+        navbar_btn(
+            "Natural & Cultural Resources", "natural-cultural-resources"
+        ),
+        navbar_btn("Local Economy", "local-economy"),
     ],
     className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
-    align="center",
+    id="navbar-links-group",
+    # align="center",
 )
 
 navbar = dbc.Navbar(
@@ -66,17 +66,14 @@ navbar = dbc.Navbar(
             navbar_brand,
             dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
             dbc.Collapse(
-                navbar_right,
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True
-            )
+                navbar_right, id="navbar-collapse", is_open=False, navbar=True
+            ),
         ],
         fluid=True,
-        class_name='px-4',
+        class_name="px-4",
     ),
     color="#ffffff",
     dark=False,
-    class_name='px-4 py-3',
+    class_name="px-4 py-3",
     # style={'box-shadow': '1px 1px 1px black'}
 )
