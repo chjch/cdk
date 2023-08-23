e749ec7436d8d4a46a775a1c501e07e9b23b59e2
Changjie Chen <chj.chen@Hotmail.com>
2023-08-22 23:04:04 -0400

    add shepherd tour css and js libraries

--- app/app.py
+++ app/app.py
@@ -4,11 +4,23 @@ import dash
 from dash import Dash, html, Input, Output
 import dash_bootstrap_components as dbc
 
+# external JS scripts
+external_scripts = [
+    {
+        "src": "https://cdnjs.cloudflare.com/ajax/libs/shepherd.js/11.1.1/js/shepherd.min.js"
+    },
+    {"src": "https://cdn.jsdelivr.net/npm/@floating-ui/core@1.3.0"},
+    {"src": "https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.3.0"},
+]
 # external CSS stylesheets
 BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
 external_stylesheets = [
-    {'src': 'https://api.tiles.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css',
-     'rel': 'stylesheet'},
+    {
+        "src": "https://api.tiles.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css",
+        "rel": "stylesheet",
+    },
+    "https://cdnjs.cloudflare.com/ajax/libs/shepherd.js/11.1.1/css/shepherd.css",
+    "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap",
     dbc.themes.MATERIA,
     # BS
 ]
@@ -16,28 +28,27 @@ external_stylesheets = [
 app = Dash(
     __name__,
     use_pages=True,
+    external_scripts=external_scripts,
     external_stylesheets=external_stylesheets,
-    suppress_callback_exceptions=True
+    suppress_callback_exceptions=True,
 )
-app.title = 'Cedar Key Dashboard'
+app.title = "Cedar Key Dashboard"
 
 server = app.server
 
-app.layout = html.Div(
-    dash.page_container
-)
+app.layout = html.Div(dash.page_container)
 
 app.clientside_callback(
     """function () {
+        setTimeout(function() {createIntroTour()}, 500);
         setTimeout(function() {addListeners()}, 1000);
         return 0;
     }""",
-    Output("intro-message", 'n_clicks'),
-    Input("line-chart", 'figure')
+    Output("intro-message", "n_clicks"),
+    Input("line-chart", "figure"),
 )
 
 if __name__ == "__main__":
     app.run_server(
-        debug=False, host="0.0.0.0",
-        port=int(os.environ.get('PORT', 8080))
+        debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))
     )
