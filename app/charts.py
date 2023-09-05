82de5d668d95ccc4d46eacb8314e2c8585481272
Changjie Chen <chj.chen@Hotmail.com>
2023-09-05 10:06:14 -0400

    update charts

--- app/charts.py
+++ app/charts.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import plotly.express as px
-from utils import scenarios
+from utils import scenarios, line_color_map, asset_axis_title
 
 
 depth_df = pd.read_csv("app/data/cdk_critical_assets_flood_depth.csv")
@@ -16,30 +16,8 @@ asset_dict = {
     "resrc": "Natural & Cultural Resources",
     "trans": "Transportation",
     "economy": "Local Economy",
-    "overall": "All Critical Assets",
+    "housing": "Housing",
 }
-
-category_orders = {
-    "Asset class": [
-        "TRANSPORTATION",
-        "CRITICAL INFRASTRUCTURE",
-        "COMMUNITY SERVICES",
-        "NATURAL & CULTURAL RESOURCES",
-        "LOCAL ECONOMY",
-    ]
-}
-
-
-def line_color_map(color_sequence_name, scenario=None, opacity=0):
-    colors = getattr(px.colors.qualitative, color_sequence_name)
-    i = 0
-    for scn in scenarios:
-        color_map[scn] = f"rgba{px.colors.hex_to_rgb(colors[i])}".replace(
-            ")", f", {opacity})"
-        )
-        i += 1
-    color_map[scenario] = color_map[scenario].replace(f"{opacity})", "1)")
-    return color_map
 
 
 def line_chart(asset: str, scenario: str):
@@ -54,14 +32,14 @@ def line_chart(asset: str, scenario: str):
         symbol="scenario",
         template="plotly_white",
         custom_data=["scenario"],
-        color_discrete_sequence=px.colors.qualitative.Plotly,
+        color_discrete_map=color_map
     )
     fig.update_xaxes(
         ticktext=["2022", "2040", "2070"],
         tickvals=[2022, 2040, 2070],
         title=None,
     )
-    fig.update_yaxes(range=[-1, 101], title="Assets inundated (%)")
+    fig.update_yaxes(range=[-1, 101], title="Total Assets Flooded (%)")
     fig.update_layout(
         # title={'text': asset_dict[asset],
         #        'font': {'size': 16},
@@ -76,30 +54,18 @@ def line_chart(asset: str, scenario: str):
 
 def bar_chart(asset: str, scenario: str, year: int):
     bar_trace = f"{scenario}_{str(year)}"
-    if asset != "overall":
-        bar_df = class_df[class_df["Asset class"] == asset]
-        x_title = f"{asset} (%) by flood depth"
-    if asset == "HOUSING":
-        bar_df = class_df
-        x_title = f"All critical assets (%) by flood depth"
+    bar_df = class_df[class_df["Asset class"] == asset]
+    x_title = f"Assets Flooded by Depth (%) under {scenario} in {year}"
     fig = px.bar(
         bar_df,
         x="Flood depth",
         y=bar_trace,
         color="Asset class",
-        barmode="group",
-        category_orders=category_orders,
-        template="plotly_white",
-        color_discrete_sequence=[
-            "#5289B0",
-            "#AAB543",
-            "#EA9731",
-            "#838383",
-            "#F6C540",
-        ],
+        template="plotly_white"
     )
     fig.update_yaxes(range=[-1, 101], title=None)
     fig.update_xaxes(title=x_title)
+    fig.update_traces(marker_color=color_map[scenario])
     fig.update_layout(
         legend=dict(
             orientation="h",
@@ -112,6 +78,14 @@ def bar_chart(asset: str, scenario: str, year: int):
         ),
         bargroupgap=0.15,
         showlegend=False,
-        margin=dict(l=0, r=0, t=0, b=0),
+        margin=dict(l=20, r=0, t=20, b=0),
+    )
+    fig.add_annotation(
+        x='>27 ft', y=85,
+        # xshift=-40,
+        text=f"{asset_axis_title[asset]}",
+        showarrow=False,
+        yshift=10,
+        font=dict(size=14)
     )
     return fig
