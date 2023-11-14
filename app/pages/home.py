add1624b1ffd42bc2ee219b86f9e672751ba6d7f
Changjie Chen <chj.chen@Hotmail.com>
2023-11-14 08:58:14 -0500

    remove repeated text in modal

--- app/pages/home.py
+++ app/pages/home.py
@@ -14,6 +14,26 @@ def toggle_modal(n1, n2, is_open):
         return not is_open
     return is_open
 
+
+disclaimer = """
+    The data and maps in this tool illustrate the scale of
+    potential flooding, not the exact location, and do not account
+    for erosion, subsidence, or future construction.
+    Although every effort has been made to ensure that information
+    is comprehensive and accurate, errors and omissions may exist.
+    The data and the information included therein is provided on an
+    "as is" basis.
+    The Florida Institute for Built Environment Resilience (FIBER),
+    Florida Sea Grant, the University of Florida, or any of their
+    respective faculty, staff, or administration specifically
+    disclaim any warranty, either expressed or implied, including
+    but not limited to the implied warranties of merchantability
+    and fitness for a particular use.
+    The entire risk as to quality and performance is with the user.
+    This tool should be used strictly as a planning reference tool
+    and not for navigation, permitting, or other legal purposes.
+"""
+
 layout = html.Div(
     children=[
         html.H1(
@@ -65,24 +85,7 @@ layout = html.Div(
             },
         ),
         html.Div(
-            children="""
-                The data and maps in this tool illustrate the scale of
-                potential flooding, not the exact location, and do not account
-                for erosion, subsidence, or future construction.
-                Although every effort has been made to ensure that information
-                is comprehensive and accurate, errors and omissions may exist.
-                The data and the information included therein is provided on an
-                "as is" basis.
-                The Florida Institute for Built Environment Resilience (FIBER),
-                Florida Sea Grant, the University of Florida, or any of their
-                respective faculty, staff, or administration specifically
-                disclaim any warranty, either expressed or implied, including
-                but not limited to the implied warranties of merchantability
-                and fitness for a particular use.
-                The entire risk as to quality and performance is with the user.
-                This tool should be used strictly as a planning reference tool
-                and not for navigation, permitting, or other legal purposes.
-            """,
+            children=disclaimer,
             className="hidden-mobile",
             style={
                 "color": "#dddddd",
@@ -104,24 +107,7 @@ layout = html.Div(
         dbc.Modal(
             [
                 dbc.ModalHeader(dbc.ModalTitle("Disclaimer"), close_button=True, className="disclaimer-header btn-close-white"),
-                dbc.ModalBody("""          
-                The data and maps in this tool illustrate the scale of
-                potential flooding, not the exact location, and do not account
-                for erosion, subsidence, or future construction.
-                Although every effort has been made to ensure that information
-                is comprehensive and accurate, errors and omissions may exist.
-                The data and the information included therein is provided on an
-                "as is" basis.
-                The Florida Institute for Built Environment Resilience (FIBER),
-                Florida Sea Grant, the University of Florida, or any of their
-                respective faculty, staff, or administration specifically
-                disclaim any warranty, either expressed or implied, including
-                but not limited to the implied warranties of merchantability
-                and fitness for a particular use.
-                The entire risk as to quality and performance is with the user.
-                This tool should be used strictly as a planning reference tool
-                and not for navigation, permitting, or other legal purposes.
-            """),
+                dbc.ModalBody(disclaimer),
                 dbc.ModalFooter(
                     dbc.Button(
                         "Close", id="close", className="ms-auto show-mobile", n_clicks=0
